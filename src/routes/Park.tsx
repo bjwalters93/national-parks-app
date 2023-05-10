f84b660a1128fa182132109b5fdecc170bcd0bf4
Brian Walters <waltersbrian68@gmail.com>
2023-05-10 08:37:48 -0400

    finished lightbox

--- src/routes/Park.tsx
+++ src/routes/Park.tsx
@@ -3,67 +3,58 @@ import "./Park.css";
 import { useLoaderData, Link } from "react-router-dom";
 import Lightbox from "yet-another-react-lightbox";
 import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+import Inline from "yet-another-react-lightbox/plugins/inline";
+import Captions from "yet-another-react-lightbox/plugins/captions";
+import Counter from "yet-another-react-lightbox/plugins/counter";
+import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
 import "yet-another-react-lightbox/styles.css";
 import "yet-another-react-lightbox/plugins/thumbnails.css";
+import "yet-another-react-lightbox/plugins/captions.css";
+import "yet-another-react-lightbox/plugins/counter.css";
 
 export async function loadPark({ params }: any) {
-  const response1 = await fetch(
+  const parkResponse = await fetch(
     `https://developer.nps.gov/api/v1/parks?parkCode=${params.park}&api_key=9JlgO9YSfRlkWXenMR8S3X3uW9uW0cZBdycA46tm`
   );
-  const parkData = await response1.json();
-
-  const response2 = await fetch(
+  const parkData = await parkResponse.json();
+  const imageResponse = await fetch(
     `https://developer.nps.gov/api/v1/multimedia/galleries?limit=1000&parkCode=${params.park}&api_key=9JlgO9YSfRlkWXenMR8S3X3uW9uW0cZBdycA46tm`
   );
-  const imageData = await response2.json();
+  const imageData = await imageResponse.json();
   return { park: parkData.data, images: imageData.data };
 }
 
 function Park() {
-  const [open, setOpen] = React.useState(false);
   const data = useLoaderData() as ReturnType<any>;
   console.log("data:", data);
-  const imagesArr1 = data.images.map((image: any, index: any) => {
-    return (
-      <div className="gallery">
-        <a target="_blank" rel="noreferrer" href={image.images[0].url}>
-          <img
-            className="image_class"
-            src={image.images[0].url}
-            alt={image.images[0].altText}
-          />
-        </a>
-        {/* <div className="desc">{image.images[0].title}</div> */}
-      </div>
-    );
-  });
-  //   const imagesArr2 = park[0].images.map((image: any, index: any) => {
-  //     return (
-  //       <div key={index}>
-  //         <img src={image.url} alt={image.altText} style={{ width: "500px" }} />
-  //       </div>
-  //     );
-  //   });
-  const imagesArr3 = data.images.map((image: any, index: any) => {
-    return { src: image.images[0].url };
+
+  const imagesArr = data.images.map((image: any, index: any) => {
+    return {
+      src: image.images[0].url,
+      alt: image.images[0].altText,
+      title: image.images[0].title,
+      description: image.images[0].description,
+    };
   });
 
-  console.log(imagesArr3);
+  console.log(imagesArr);
 
   return (
     <div className="Park">
       <h1>{data.park[0].fullName}</h1>
       <p>{data.park[0].description}</p>
-      {/* <div className="gallery_container">{imagesArr1}</div> */}
-      <button type="button" onClick={() => setOpen(true)}>
-        Open Lightbox
-      </button>
-
       <Lightbox
-        plugins={[Thumbnails]}
-        open={open}
-        close={() => setOpen(false)}
-        slides={[...imagesArr3]}
+        plugins={[Inline, Thumbnails, Captions, Counter, Fullscreen]}
+        counter={{ style: { top: 24 } }}
+        inline={{
+          style: {
+            margin: "0 auto",
+            width: "100%",
+            maxWidth: "800px",
+            aspectRatio: "3 / 2",
+          },
+        }}
+        slides={[...imagesArr]}
       />
     </div>
   );
