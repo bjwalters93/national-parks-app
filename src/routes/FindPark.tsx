1f9127557dc6b92c3c55c6fcb56bd1b3f973197f
Brian Walters <waltersbrian68@gmail.com>
2023-05-17 14:15:12 -0400

    working on find park page mq

--- src/routes/FindPark.tsx
+++ src/routes/FindPark.tsx
@@ -39,22 +39,65 @@ function imageError(event: React.SyntheticEvent<HTMLImageElement, Event>) {
 
 function FindPark() {
   const FindPark_LD = useLoaderData() as parkListData;
+  console.log(FindPark_LD);
   let parkArr: React.ReactElement[] = [];
+  //   if (FindPark_LD !== null) {
+  //     parkArr = FindPark_LD.parkList.map((park) => {
+  //       return (
+  //         <li className="li_find_park" key={park.parkCode}>
+  //           <div className="lazy_load_container">
+  //             <LazyLoad height={125} width={125} threshold={0.1}>
+  //               <img
+  //                 src={park.images[0].url}
+  //                 alt={park.images[0].altText}
+  //                 className="li_find_park_image"
+  //                 onError={imageError}
+  //               />
+  //             </LazyLoad>
+  //           </div>
+  //           <div>
+  //             <h3 className="li_titles">
+  //               <Link
+  //                 className="park_links"
+  //                 to={"/park/" + FindPark_LD.stateCode + "/" + park.parkCode}
+  //               >
+  //                 {park.fullName}
+  //               </Link>
+  //             </h3>
+  //             <h4 className="li_designation">{park.designation}</h4>
+  //             <p>
+  //               <span>States:</span>{" "}
+  //               {park.states
+  //                 .split(",")
+  //                 .map((el) => {
+  //                   let state = stateCodes.find(({ code }) => code === el) as {
+  //                     name: string;
+  //                   };
+  //                   return state.name;
+  //                 })
+  //                 .join(", ")}
+  //             </p>
+  //             <p>{park.description}</p>
+  //           </div>
+  //         </li>
+  //       );
+  //     });
+  //   }
   if (FindPark_LD !== null) {
     parkArr = FindPark_LD.parkList.map((park) => {
       return (
         <li className="li_find_park" key={park.parkCode}>
-          <div style={{ margin: "0 50px 0 0" }}>
-            <LazyLoad height={125} width={125} threshold={0.1}>
-              <img
-                src={park.images[0].url}
-                alt={park.images[0].altText}
-                className="li_find_park_image"
-                onError={imageError}
-              />
-            </LazyLoad>
-          </div>
           <div>
+            <div className="lazy_load_container">
+              <LazyLoad height={75} width={75} threshold={0.1}>
+                <img
+                  src={park.images[0].url}
+                  alt={park.images[0].altText}
+                  className="li_find_park_image"
+                  onError={imageError}
+                />
+              </LazyLoad>
+            </div>
             <h3 className="li_titles">
               <Link
                 className="park_links"
