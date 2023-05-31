c3e7d588f1ba0014a68f72d1fe38c83c2ac8b168
Brian Walters <waltersbrian68@gmail.com>
2023-05-31 07:54:42 -0400

    finsished alerts

--- src/routes/Alerts.tsx
+++ src/routes/Alerts.tsx
@@ -2,7 +2,16 @@ import * as React from "react";
 import "./Alerts.css";
 import { Params, useLoaderData } from "react-router-dom";
 
-type alertsData = {};
+type alertsData = {
+  alerts: {
+    category: string;
+    description: string;
+    id: string;
+    lastIndexedDate: string;
+    title: string;
+    url: string;
+  }[];
+};
 
 export async function loadAlerts({
   params,
@@ -19,9 +28,33 @@ export async function loadAlerts({
 function Alerts() {
   const Alerts_LD = useLoaderData() as alertsData;
   console.log("Alerts_LD:", Alerts_LD);
+  let alertsArr: React.ReactElement[] = Alerts_LD.alerts.map((alert) => {
+    return (
+      <div className="alert_container" key={alert.id}>
+        <h2>{alert.title}</h2>
+        <p>{alert.lastIndexedDate}</p>
+        <p>
+          <span className="span_bold_1_AL">Category: </span>
+          {alert.category}
+        </p>
+        <p>
+          <span className="span_bold_1_AL">Description: </span>
+          {alert.description}
+        </p>
+        <a href={alert.url} target="_blank" rel="noreferrer">
+          More Information
+        </a>
+      </div>
+    );
+  });
   return (
     <div className="Alerts">
       <h1 className="alerts__title">Alerts</h1>
+      {Alerts_LD.alerts.length > 0 ? (
+        <div className="alerts_list_container">{alertsArr}</div>
+      ) : (
+        <p>There are currently no alerts for this park.</p>
+      )}
     </div>
   );
 }
